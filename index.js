08f58219910843607efda1dfa655a25c0f9e01d1
Quentin Hermiteau <quentinhermiteau@gmail.com>
2020-12-26 13:36:54 +0100

    :tada: Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const button = document.getElementById('button');
+
+button.onclick = function onClickButton() {
+    const n1 = document.getElementById('n1').value;
+    const operator = document.getElementById('operator').value;
+    const n2 = document.getElementById('n2').value;
+};
