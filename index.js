2aebbda375624d5b9591756a93c5dec41883b32a
Thomas Corio <Thomas.corio91@gmail.com>
2021-01-02 20:57:56 +0100

    Ajouter le résultat de l'opération

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ button.onclick = function onClickButton() {
     const n1 = document.getElementById('n1').value;
     const operator = document.getElementById('operator').value;
     const n2 = document.getElementById('n2').value;
+    document.getElementById('result').textContent = eval(n1 + operator + n2);
 };
